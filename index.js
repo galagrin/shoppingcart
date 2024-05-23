f63ee9002043eab2970f3a52ac74fe2fcf8aac7f
Galina Bobrik <galagrinko@gmail.com>
2024-05-23 17:15:13 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const priceFirst = document.getElementById('priceFirst');
+const priceSecond = document.getElementById('priceSecond');
+const priceThird = document.getElementById('priceThird');
+const priceForth = document.getElementById('priceForth');
+const discountButton = document.getElementById('discountbutton');
+
+
+const totalPrice = +priceFirst.innerHTML + +priceSecond.innerHTML + +priceForth.innerHTML+ +priceForth.innerHTML;
+console.log(totalPrice);
+
+const priceOutput = document.querySelector('.totalprice');
+priceOutput.textContent = 'Итого: ' + totalPrice.toLocaleString('ru-RU')+ ' руб.'; 
+
+
+
+function addDiscount (){
+    const discountPrice = totalPrice * 0.8;
+    priceOutput.textContent = 'Итого со скидкой: ' + discountPrice.toLocaleString('ru-RU')+ ' руб.'; 
+};
+
+discountButton.addEventListener ('click', addDiscount);
